34d280bd5ae37743a5afb4d28a44bdba40ee5c01
Mike Schwartz <mikeschwartz@quickenloans.com>
2020-03-24 16:54:30 -0400

    first commit

---
+++ Lab11_2_ShopForm/Controllers/RegisterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab11_2_ShopForm.wwwroot
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register(string fname, string lname, string email, string phone, string uname, string password)
+        {
+            ViewData["name"] = fname + " " + lname;
+            ViewData["email"] = email;
+            ViewData["passwod"] = password;
+            ViewData["Phone"] = phone;
+            ViewData["username"] = uname;
+            return View();
+        }
+    }
+}
---
+++ Lab11_2_ShopForm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab11_2_ShopForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
